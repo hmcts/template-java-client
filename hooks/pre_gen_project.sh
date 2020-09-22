0986c486c97f2463f20cae31c402f17b587b19e2
Praveen Adusumilli <praveen.adusumilli@hmcts.net>
2020-09-22 08:42:01 +0100

    typo

--- hooks/pre_gen_project.sh
+++ hooks/pre_gen_project.sh
@@ -4,7 +4,7 @@ GENERATOR_VERSION=4.3.1
 wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/${GENERATOR_VERSION}/openapi-generator-cli-${GENERATOR_VERSION}.jar -O openapi-generator-cli.jar
 
 java -jar openapi-generator-cli.jar generate \
--i {{cookiecutter. openapi_url}} \
+-i {{cookiecutter.openapi_url}} \
 --api-package {{cookiecutter.api_package}} \
 --model-package {{cookiecutter.model_package}} \
 --invoker-package {{cookiecutter.invoker_package}} \
--- {{cookiecutter.component_id}}/bin/code-gen.sh
+++ {{cookiecutter.component_id}}/bin/code-gen.sh
@@ -4,7 +4,7 @@ GENERATOR_VERSION=4.3.1
 wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/${GENERATOR_VERSION}/openapi-generator-cli-${GENERATOR_VERSION}.jar -O /tmp/openapi-generator-cli.jar
 
 java -jar /tmp/openapi-generator-cli.jar generate \
--i {{cookiecutter. openapi_url}} \
+-i {{cookiecutter.openapi_url}} \
 --api-package {{cookiecutter.api_package}} \
 --model-package {{cookiecutter.model_package}} \
 --invoker-package {{cookiecutter.invoker_package}} \
