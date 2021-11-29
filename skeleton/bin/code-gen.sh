87963ee2cf4fe321872877589b5a70e0255fe07f
Tim Jacomb <21194782+timja@users.noreply.github.com>
2021-11-29 14:37:30 +0000

    DTSPO-5580 Make script quieter fetching jar (#52)

--- skeleton/bin/code-gen.sh
+++ skeleton/bin/code-gen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 GENERATOR_VERSION=5.3.0
-wget https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/${GENERATOR_VERSION}/openapi-generator-cli-${GENERATOR_VERSION}.jar -O openapi-generator-cli.jar
+wget -q https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/${GENERATOR_VERSION}/openapi-generator-cli-${GENERATOR_VERSION}.jar -O openapi-generator-cli.jar
 
 BASE_PACKAGE=${{ values.group_id }}.${{ values.artifact_id }}
 
